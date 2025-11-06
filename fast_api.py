f2e2e82c4f1a1ff77cf592eba2dd0c22e49ab0c6
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2025-11-06 21:48:53 +0530

    url encode for vlc link

--- fast_api.py
+++ fast_api.py
@@ -4,6 +4,7 @@ import asyncio
 import mimetypes
 import os
 import logging
+from urllib.parse import quote
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import StreamingResponse, JSONResponse, RedirectResponse, FileResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -218,12 +219,13 @@ async def get_file_details(file_link: str):
 @api.get("/play/{player}/{file_link}")
 async def play_in_player(player: str, file_link: str):
     stream_url = f"{MY_DOMAIN}/stream/{file_link}"
+    encoded_url = quote(f"{MY_DOMAIN}/stream/{file_link}", safe='')
     if player == "mx":
         redirect_url = f"intent:{stream_url}#Intent;action=android.intent.action.VIEW;type=video/*;package=com.mxtech.videoplayer.ad;end"
     elif player == "mxpro":
         redirect_url = f"intent:{stream_url}#Intent;action=android.intent.action.VIEW;type=video/*;package=com.mxtech.videoplayer.pro;end"
     elif player == "vlcpc":
-        redirect_url = f"vlc://open,{MY_DOMAIN}/stream/{file_link}"
+        redirect_url = f"vlc://open,{encoded_url}"
     else:
         raise HTTPException(status_code=404, detail="Player not supported")
     return RedirectResponse(url=redirect_url, status_code=302)
