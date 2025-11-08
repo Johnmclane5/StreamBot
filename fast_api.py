0105964525d0755f31f1acc99a937504c955c281
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2025-11-08 13:47:04 +0530

    Update fast_api.py

--- fast_api.py
+++ fast_api.py
@@ -260,6 +260,8 @@ async def play_in_player(player: str, file_link: str):
         redirect_url = f"intent:{stream_url}#Intent;action=android.intent.action.VIEW;type=video/*;package=com.mxtech.videoplayer.ad;end"
     elif player == "mxpro":
         redirect_url = f"intent:{stream_url}#Intent;action=android.intent.action.VIEW;type=video/*;package=com.mxtech.videoplayer.pro;end"
+    elif player == "vlc":
+        redirect_url = f"intent:{stream_url}#Intent;action=android.intent.action.VIEW;type=video/*;package=com.videolan.vlc;end"
     else:
         raise HTTPException(status_code=404, detail="Player not supported")
     return RedirectResponse(url=redirect_url, status_code=302)
